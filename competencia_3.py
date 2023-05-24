43ade03f2c190fc537c6b781416d1b086168904a
chrodri <131100744+chrodri@users.noreply.github.com>
2023-05-24 12:22:51 -0600

    Add files via upload

---
+++ competencia_3.py
@@ -0,0 +1,28 @@
+datos = { "nombres":"Christian Rodrigo", "primer_apellido":"Delgado", "segundo_apellido":"Diaz", "fecha_nacimiento":"06-02-1993",
+         "celular":"3339031364", "correo":"christian.rodrigo.delgado@hpe.com", "domicilio":"Castilla de la mancha 68", "genero":"masculino",
+         "objetivo":"obtener nuevas herramientas para desarrollar mi trabajo mejor", "salario_esperado":"un millon dls" }
+
+
+print(datos["nombres"])
+print(datos["primer_apellido"])
+print(datos["segundo_apellido"])
+print(datos["fecha_nacimiento"])
+print(datos["celular"])
+print(datos["correo"])
+print(datos["domicilio"])
+print(datos["genero"])
+print(datos["objetivo"])
+print(datos["salario_esperado"])
+
+skills = ["microsoft office" , "Analisis de datos" , "Trabajo en equipo" , "Resolucion de problemas" , "Proactivo" , 
+          "Responsable" , "Multitasking" , "SAP"]
+
+print(skills)
+
+trabajos = { "lugar_trabajo":"Banorte", "año_inicio":"2015", "año_fin":"2017", "puesto":"atencion al cliente"}
+
+print(trabajos["lugar_trabajo"])
+print(trabajos["año_inicio"])
+print(trabajos["año_fin"])
+print(trabajos["puesto"])
+
